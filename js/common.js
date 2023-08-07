285b0bec5f3eca077799218e741e394d04368d9b
김성은 <kse012050@naver.com>
2023-08-07 13:14:31 +0900

    적립금 페이지 완료

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    // 콤마
+    $('[data-comma]').length && comma()
+    
+    // 텝
+    $('.tabBtns').length && tab()
+})
+
+
+// 콤마
+function comma(){
+    $('[data-comma]').html(addCommas($('[data-comma]').html()));
+}
+
+// 숫자 ',' 찍기
+function addCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// 텝
+function tab(){
+    $('.tabBtns > *, .tabContents > *').first().addClass('active');
+    $('.tabBtns > *').click(function(){
+        $('.tabBtns > *, .tabContents > *').removeClass('active');
+        $(this).addClass('active');
+        $('.tabContents > *').eq($(this).index()).addClass('active');
+    })
+}
